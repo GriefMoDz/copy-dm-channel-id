2726d8a67cc6bf7325f1673772f78847d1d8f7a6
Harley <GriefMoDz@gmail.com>
2020-11-05 12:45:21 +1100

    [v1.1.0] Might as well show this plug-in some love

--- index.js
+++ index.js
@@ -1,50 +1,31 @@
 const { Plugin } = require('powercord/entities');
-const { React, getModule, contextMenu } = require('powercord/webpack');
+const { React, getModule } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+const { Menu } = require('powercord/components');
 const { clipboard } = require('electron');
 
 module.exports = class CopyDMChannelID extends Plugin {
-  startPlugin () {
-    this._patchDeveloperModeGroup();
-  }
-
-  pluginWillUnload () {
-    this._unpatchDeveloperModeGroup();
-  }
-
-  async _patchDeveloperModeGroup () {
+  async startPlugin () {
     const { getDMFromUserId } = await getModule([ 'getDMFromUserId' ]);
-    const DeveloperModeGroup = await getModule(m => m.default && m.default.displayName === 'DeveloperModeGroup');
 
-    DeveloperModeGroup.default = (originalMethod => (args) => {
-      const res = originalMethod(args);
-      if (!res) {
-        return null;
-      }
-
-      const channelId = getDMFromUserId(args.id);
-      const ButtonMenuItem = res.props.children.type;
-
+    const CopyIDItem = await getModule(m => m.default && m.default.displayName === 'useCopyIdItem');
+    inject('copy-dm-channel-id', CopyIDItem, 'default', (args, res) => {
+      const channelId = getDMFromUserId(args[0]);
       if (channelId) {
-        res.props.children = [ res.props.children ];
-        res.props.children.push(React.createElement(ButtonMenuItem, {
+        return [ res, React.createElement(Menu.MenuItem, {
+          id: 'copy-dm-channel-id',
           label: 'Copy DM Channel ID',
-          action: () => {
-            contextMenu.closeContextMenu();
-            clipboard.writeText(channelId);
-          }
-        }));
+          action: () => clipboard.writeText(channelId)
+        }) ];
       }
 
       return res;
-    })(DeveloperModeGroup.__default = DeveloperModeGroup.default);
+    });
 
-    DeveloperModeGroup.default.displayName = 'DeveloperModeGroup';
+    CopyIDItem.default.displayName = 'useCopyIdItem';
   }
 
-  async _unpatchDeveloperModeGroup () {
-    const DeveloperModeGroup = await getModule(m => m.default && m.default.displayName === 'DeveloperModeGroup');
-
-    DeveloperModeGroup.default = DeveloperModeGroup.__default;
-    delete DeveloperModeGroup.__default;
+  pluginWillUnload () {
+    uninject('copy-dm-channel-id');
   }
 };
