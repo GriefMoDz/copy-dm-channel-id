5191a16b94811b8469a3e38ac719b250129a533d
Harley <GriefMoDz@gmail.com>
2020-11-07 18:32:20 +1100

    [v1.2.0] Add a console setting for button isolation

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# copy-dm-channel-id
-A Powercord plug-in that appends a new action button—\"Copy DM Channel ID\"—under the contextual developer mode group and copies the channel ID of a DM ("Direct Message") when pressed; useful for experimental purposes and/or plug-in development.
+# Copy DM Channel ID
+A Powercord plug-in that appends a new action button—\"Copy DM Channel ID\"—under the contextual developer mode group that copies the channel ID of a DM ("Direct Message") when pressed; useful for experimental purposes and/or plug-in development.
+
+## Button Isolation
+If you prefer to isolate the button to the "DM User Context Menu" you can type:
+```js
+powercord.pluginManager.get('copy-dm-channel-id').setIsolated(true);
+```
+under your DevTools console (<kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>I</kbd>). You can also reverse this by changing the value to `false` which will display it on any user context menu.
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const { Plugin } = require('powercord/entities');
 const { React, getModule } = require('powercord/webpack');
 const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
 const { Menu } = require('powercord/components');
+
 const { clipboard } = require('electron');
 
 module.exports = class CopyDMChannelID extends Plugin {
   async startPlugin () {
     const { getDMFromUserId } = await getModule([ 'getDMFromUserId' ]);
+    const useCopyDmChannelIdItem = (channelId) => React.createElement(Menu.MenuItem, {
+      id: 'copy-dm-channel-id',
+      label: 'Copy DM Channel ID',
+      action: () => clipboard.writeText(channelId)
+    });
+
+    const DMUserContextMenu = await getModule(m => m.default && m.default.displayName === 'DMUserContextMenu');
+    inject('copy-dm-channel-id-isolated', DMUserContextMenu, 'default', (args, res) => {
+      const developerGroup = findInReactTree(res, n => n.props && n.props.children && n.props.children.key === 'devmode-copy-id');
+      if (developerGroup && this.settings.get('isolated', false)) {
+        if (!Array.isArray(developerGroup)) {
+          developerGroup.props.children = [ developerGroup.props.children ];
+        }
+
+        developerGroup.props.children.push(useCopyDmChannelIdItem(args[0].channel.id));
+      }
+
+      return res;
+    });
+
+    DMUserContextMenu.default.displayName = 'DMUserContextMenu';
 
     const CopyIDItem = await getModule(m => m.default && m.default.displayName === 'useCopyIdItem');
-    inject('copy-dm-channel-id', CopyIDItem, 'default', (args, res) => {
+    inject('copy-dm-channel-id-global', CopyIDItem, 'default', (args, res) => {
       const channelId = getDMFromUserId(args[0]);
-      if (channelId) {
-        return [ res, React.createElement(Menu.MenuItem, {
-          id: 'copy-dm-channel-id',
-          label: 'Copy DM Channel ID',
-          action: () => clipboard.writeText(channelId)
-        }) ];
+      if (channelId && !this.settings.get('isolated', false)) {
+        return [ res, useCopyDmChannelIdItem(channelId) ];
       }
 
       return res;
@@ -26,6 +45,13 @@ module.exports = class CopyDMChannelID extends Plugin {
   }
 
   pluginWillUnload () {
-    uninject('copy-dm-channel-id');
+    uninject('copy-dm-channel-id-isolated');
+    uninject('copy-dm-channel-id-global');
+  }
+
+  setIsolated (state) {
+    this.settings.set('isolated', state);
+
+    return this.log(`Copy DM Channel ID ${state ? 'has now been' : 'is no longer'} isolated to the DM User Context Menu!`);
   }
 };
